bb56cceff1a03a6c594428d6b28178f25d29412b
dylancdavis <dylcdav@gmail.com>
2023-04-05 20:08:35 -0400

    Fix Blog.test.js

--- frontend/src/tests/Blog.test.js
+++ frontend/src/tests/Blog.test.js
@@ -1,93 +1,33 @@
-import Blog from "../components/Blog";
+import BlogItem from "../components/BlogItem";
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom/extend-expect";
-import userEvent from "@testing-library/user-event";
 
 const sampleBlog = {
+  id: 1,
   title: "A Sample Blog",
   author: "Example Author",
   likes: 0,
   url: "www.example.com",
   user: {
-    name: "Example Name",
+    username: "Example Name",
   },
 };
 
-let mockDelete;
-
-describe("Blog component", () => {
-  beforeEach(() => {
-    mockDelete = jest.fn();
-  });
-
+describe("BlogItem", () => {
   test("renders title and author", () => {
-    const mockLike = jest.fn();
     const { container } = render(
-      <Blog
-        blog={sampleBlog}
-        handleBlogLike={mockLike}
-        handleBlogDelete={mockDelete}
-      />
+      <MemoryRouter>
+        <table>
+          <tbody>
+            <BlogItem blog={sampleBlog} />
+          </tbody>
+        </table>
+      </MemoryRouter>
     );
     const blogElement = container.querySelector(".blog");
     expect(blogElement).toHaveTextContent(sampleBlog.title);
     expect(blogElement).toHaveTextContent(sampleBlog.author);
-  });
-
-  test("does not nitially render likes or URL", () => {
-    const mockLike = jest.fn();
-    render(
-      <Blog
-        blog={sampleBlog}
-        handleBlogLike={mockLike}
-        handleBlogDelete={mockDelete}
-      />
-    );
-    expect(screen.queryByText(`-Likes: ${sampleBlog.likes}`)).toBeNull();
-    expect(screen.queryByText(`URL: ${sampleBlog.url}`)).toBeNull();
-  });
-
-  test("renders likes and URL on show button click", async () => {
-    const mockLike = jest.fn();
-    const { container } = render(
-      <Blog
-        blog={sampleBlog}
-        handleBlogLike={mockLike}
-        handleBlogDelete={mockDelete}
-      />
-    );
-
-    // Click the 'show' button
-    const user = userEvent.setup();
-    const showBtn = container.querySelector(".show-button");
-    await user.click(showBtn);
-
-    // See if the text exists after buttonclick
-    screen.getByText(`-Likes: ${sampleBlog.likes}`);
-    screen.getByText(`URL: ${sampleBlog.url}`);
-  });
-
-  test("calls like handler twice when like button clicked twice", async () => {
-    const mockLike = jest.fn();
-    const { container } = render(
-      <Blog
-        blog={sampleBlog}
-        handleBlogLike={mockLike}
-        handleBlogDelete={mockDelete}
-      />
-    );
-    const user = userEvent.setup();
-
-    // Show content for like button
-    const showBtn = container.querySelector(".show-button");
-    await user.click(showBtn);
-
-    // Click like button twice
-    const likeBtn = container.querySelector(".like-button");
-    await user.click(likeBtn);
-    await user.click(likeBtn);
-
-    expect(mockLike.mock.calls).toHaveLength(2);
   });
 });
