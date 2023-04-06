688e7d478051ecb04463b35517f30c5a111505bf
dylancdavis <dylcdav@gmail.com>
2023-04-06 12:58:34 -0400

    Add wildcard to login routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -132,7 +132,7 @@ const App = () => {
       ) : (
         <Routes>
           <Route
-            path="/"
+            path="/*"
             element={<Login notificationMessage={notificationMessage} />}
           />
           <Route
