2b6f97864650c653ded4af24fc2f9f5f1ce744cc
Cole Davis <dylcdav@gmail.com>
2025-05-07 18:38:47 -0400

    Run formatter

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,15 @@ const http = require("http");
 const express = require("express");
 require("express-async-errors");
 const app = express();
-const config = require("./utils/config");
+const config = require("./utils/config.js");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const blogsRouter = require("./controllers/blogs");
-const userRouter = require("./controllers/users");
-const loginRouter = require("./controllers/login");
-const tokenFromReqest = require("./utils/tokenExtractor");
+const blogsRouter = require("./controllers/blogs.js");
+const userRouter = require("./controllers/users.js");
+const loginRouter = require("./controllers/login.js");
+const tokenFromReqest = require("./utils/tokenExtractor.js");
 const errorHandler = require("./utils/errorHandler.js");
-const userExtractor = require("./utils/userExtractor");
+const userExtractor = require("./utils/userExtractor.js");
 const morgan = require("morgan");
 
 async function connectToDatabase() {
@@ -23,6 +23,8 @@ async function connectToDatabase() {
     await mongoose.connect(config.MONGO_URI);
   }
 }
+
+connectToDatabase();
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -37,7 +39,7 @@ app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
 
 if (process.env.NODE_ENV === "test") {
-  const testingRouter = require("./controllers/testing");
+  const testingRouter = require("./controllers/testing.js");
   app.use("/api/testing", testingRouter);
 }
 
